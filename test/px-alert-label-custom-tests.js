0084dcad1910f87ccc881d4ae6968d898e274ee0
Randy Askin <randall.askin@ge.com>
2017-11-01 13:23:54 -0700

    polymer 2.0 preview

--- test/px-alert-label-custom-tests.js
+++ test/px-alert-label-custom-tests.js
@@ -1,50 +1,43 @@
-// This is the wrapper for custom tests, called upon web components ready state
-function runCustomTests() {
-  // Place any setup steps like variable declaration and initialization here
-
-  // This is the placeholder suite to place custom tests in
-  // Use testCase(options) for a more convenient setup of the test cases
-  suite('Custom Automation Tests for px-alert-label', function() {
-    test('Important alert has the correct label text', function(done) {
-      var fixtureEl = document.getElementById('fixture-important');
-      assert.equal(fixtureEl.label, 'Important');
-      done();
-    });
-
-    test('Warning alert has the correct label text', function(done) {
-      var fixtureEl = document.getElementById('fixture-warning');
-      assert.equal(fixtureEl.label, 'Warning');
-      done();
-    });
-
-    test('Information alert has the correct label text', function(done) {
-      var fixtureEl = document.getElementById('fixture-info');
-      assert.equal(fixtureEl.label, 'Information');
-      done();
-    });
-
-    test('Healthy alert has the correct label text', function(done) {
-      var fixtureEl = document.getElementById('fixture-healthy');
-      assert.equal(fixtureEl.label, 'Healthy');
-      done();
-    });
-
-    test('Inverted alert has the correct label text', function(done) {
-      var fixtureEl = document.getElementById('fixture-healthy-inverted');
-      assert.equal(fixtureEl.label, 'Inverted');
-      done();
-    });
-
-    test('Unknown alert has the correct label text', function(done) {
-      var fixtureEl = document.getElementById('fixture-unknown');
-      assert.equal(fixtureEl.label, 'Unknown');
-      done();
-    });
-
-    test('Badge alert has the correct label text', function(done) {
-      var fixtureEl = document.getElementById('fixture-badge');
-      assert.equal(fixtureEl.label, '3');
-      done();
-    });
+suite('Custom Automation Tests for px-alert-label', function() {
+  test('Important alert has the correct label text', function(done) {
+    var labelElement = fixture('fixture-important');
+    assert.equal(labelElement.label, 'Important');
+    done();
   });
-};
+
+  test('Warning alert has the correct label text', function(done) {
+    var labelElement = fixture('fixture-warning');
+    assert.equal(labelElement.label, 'Warning');
+    done();
+  });
+
+  test('Information alert has the correct label text', function(done) {
+    var labelElement = fixture('fixture-info');
+    assert.equal(labelElement.label, 'Information');
+    done();
+  });
+
+  test('Healthy alert has the correct label text', function(done) {
+    var labelElement = fixture('fixture-healthy');
+    assert.equal(labelElement.label, 'Healthy');
+    done();
+  });
+
+  test('Inverted alert has the correct label text', function(done) {
+    var labelElement = fixture('fixture-healthy-inverted');
+    assert.equal(labelElement.label, 'Inverted');
+    done();
+  });
+
+  test('Unknown alert has the correct label text', function(done) {
+    var labelElement = fixture('fixture-unknown');
+    assert.equal(labelElement.label, 'Unknown');
+    done();
+  });
+
+  test('Badge alert has the correct label text', function(done) {
+    var labelElement = fixture('fixture-badge');
+    assert.equal(labelElement.label, '3');
+    done();
+  });
+});
